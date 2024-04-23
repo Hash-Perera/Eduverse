742fa423e0d84509003dd41d7e37b9375d0db413
Hashan Perera <hashperera.v@gmail.com>
2024-04-23 19:11:18 +0530

    complete auth middleware

--- backend/MS-Course-Management/src/routes/course.routes.js
+++ backend/MS-Course-Management/src/routes/course.routes.js
@@ -1,5 +1,6 @@
 const CourseService = require("../services/course.service");
 const { SubscribeMessages } = require("../utils/index.utils");
+const AuthMiddleware = require("../middlewares/auth.middleware");
 
 module.exports = (app, channel) => {
   const service = new CourseService();
@@ -8,8 +9,11 @@ module.exports = (app, channel) => {
   SubscribeMessages(channel, service);
 
   //Other routes
-  app.post("/createCourse", async (req, res) => {
+  //TODO: complete this functions
+  app.post("/createCourse", AuthMiddleware, async (req, res) => {
     //const result = await service.CreateCourse(req.body, res);
+    console.log(req.body);
+    console.log(req.user);
     res.send({ msg: "course created" });
   });
 };
