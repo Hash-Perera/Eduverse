d164855d4d8a6d0029225fa9e80e77317568eeb5
Chathura_Janithya <17chathurajanithya@gmail.com>
2024-05-06 20:29:03 +0530

    chathura | course / lessons

--- backend/MS-Course-Management/src/schema/course.schema.js
+++ backend/MS-Course-Management/src/schema/course.schema.js
@@ -5,11 +5,41 @@ const courseSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-
   price: {
     type: String,
     required: true,
   },
+  duration: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: String,
+    required: true,
+  },
+  sampleImage: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  lessons: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Lesson",
+    },
+  ],
+  status: {
+    type: String,
+    default: "pending",
+    required: true,
+  },
 });
 
-export default mongoose.model("Course", courseSchema);
+module.exports = mongoose.model("Course", courseSchema);
--- backend/MS-Course-Management/src/services/course.service.js
+++ backend/MS-Course-Management/src/services/course.service.js
@@ -1,3 +1,4 @@
+const Course = require("../schema/course.schema");
 class CourseService {
   //? This is remote service function
   async SubscribeEvents(payload) {
@@ -5,7 +6,7 @@ class CourseService {
 
     switch (payload.event) {
       case "CREATE_COURSE":
-        this.CreateCourseTest(payload);
+        this.CreateCourse(payload);
         break;
       case "GET_COURSES":
         this.R_getCourses();
@@ -16,11 +17,138 @@ class CourseService {
   }
 
   //TODO: Implement other service functions here ---------------------------------
-  async CreateCourseTest(payload) {
+  /* async CreateCourseTest(payload) {
     console.log("Create course test");
     console.log(payload.data);
+  } */
+
+  // Create course
+  async CreateCourse(payload, res) {
+    const newCourse = await Course.create(payload);
+    res.status(200).send({
+      success: true,
+      data: newCourse,
+      message: "Course created successfully",
+    });
   }
 
+  //course filter with params category and status
+  async GetCourseFilters(payload) {
+    if (payload.category === "all") {
+      const courses = await Course.find({ status: payload.status })
+        .populate({
+          path: "lessons",
+          model: "Lesson",
+        })
+        .exec();
+
+      if (!courses) {
+        return res.status(404).send({
+          success: false,
+          message: "No courses found",
+        });
+      }
+      return courses;
+    } else {
+      const courses = await Course.find({
+        category: payload.category,
+        status: payload.status,
+      })
+        .populate({
+          path: "lessons",
+          model: "Lesson",
+        })
+        .exec();
+
+      if (!courses) {
+        return res.status(404).send({
+          success: false,
+          message: "No courses found",
+        });
+      }
+      return courses;
+    }
+  }
+
+  //get course by id
+  async GetCourseById(payload) {
+    const course = await Course.findById({ _id: payload.id })
+      .populate({
+        path: "lessons",
+        model: "Lesson",
+      })
+      .exec();
+
+    if (!course) {
+      return res.status(404).send({
+        success: false,
+        message: "Course not found",
+      });
+    }
+    return course;
+  }
+
+  //update course status
+  async UpdateCourseStatus(payload, res) {
+    const course = await Course.findOneAndUpdate(
+      { _id: payload.id },
+      { status: payload.status },
+      { new: true }
+    );
+
+    if (!course) {
+      return res.status(404).send({
+        success: false,
+        message: "Course not found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      data: course,
+      message: "Course Status Updated successfully",
+    });
+  }
+
+  //update course details
+  async UpdateCourseDetails(payload, res) {
+    const course = await Course.findOneAndUpdate(
+      { _id: payload.id },
+      {
+        name: payload.name,
+        price: payload.price,
+        duration: payload.duration,
+        description: payload.description,
+        instructor: payload.instructor,
+        sampleImage: payload.sampleImage,
+        category: payload.category,
+      },
+      { new: true }
+    );
+
+    res.status(200).send({
+      success: true,
+      data: course,
+      message: "Course Updated successfully",
+    });
+  }
+
+  //delete course
+  async DeleteCourse(payload, res) {
+    const course = await Course.findByIdAndDelete(payload.id);
+
+    if (!course) {
+      return res.status(404).send({
+        success: false,
+        message: "Course not found",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      data: course,
+      message: "Course deleted successfully",
+    });
+  }
   //! =======  DO not Delete this function =========
   async R_getCourses() {
     console.log("Get courses list");
