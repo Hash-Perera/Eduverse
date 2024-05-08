4ba8f06859a1d109c4ea92ab032f48aff72dcfe6
Chathura_Janithya <17chathurajanithya@gmail.com>
2024-05-08 23:11:46 +0530

    chathura | multerconfig course img upload

--- backend/MS-Course-Management/src/routes/course.routes.js
+++ backend/MS-Course-Management/src/routes/course.routes.js
@@ -1,6 +1,6 @@
 const CourseService = require("../services/course.service");
 const { SubscribeMessages } = require("../utils/index.utils");
-
+const upload = require("../utils/multerConfig");
 module.exports = (app, channel) => {
   const service = new CourseService();
   const baseUrl = "/course";
@@ -10,10 +10,10 @@ module.exports = (app, channel) => {
 
   //Other routes
   //TODO: complete this functions
-  app.post(`${baseUrl}/create`, async (req, res) => {
+  /*   app.post(`${baseUrl}/create`, async (req, res) => {
     const result = await service.CreateCourse(req.body, res);
     res.send(result);
-  });
+  }); */
 
   app.put(`${baseUrl}/status-update`, async (req, res) => {
     const result = await service.UpdateCourseStatus(req.body, res);
@@ -34,12 +34,23 @@ module.exports = (app, channel) => {
   //get course with filters
   app.get(`${baseUrl}/filters`, async (req, res) => {
     const result = await service.GetCourseFilters(req.query);
-    res.send(result);
+    res.send(result.length > 0 ? result : "No courses found");
   });
 
   //delete course
   app.delete(`${baseUrl}/delete`, async (req, res) => {
     const result = await service.DeleteCourse(req.body, res);
+    res.send(result);
+  });
+
+  // Route to handle file upload
+  app.post(`${baseUrl}/upload`, upload.single("image"), async (req, res) => {
+    //req body with image file path
+    const body = {
+      ...req.body,
+      image: req.file.filename,
+    };
+    const result = await service.CreateCourse(body, res);
     res.send(result);
   });
 };
